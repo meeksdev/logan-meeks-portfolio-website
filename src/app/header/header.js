2cec7a604f8cf4f489f2e59fc16035b9c8fff30b
OneLessPuppy <21loganmeeks@gmail.com>
2024-11-11 21:12:50 -0800

    Added scroll spy behavior to dynamically change nav menu styles.

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -9,33 +9,46 @@ import {
     NavigationMenuViewport,
 } from '@/components/ui/navigation-menu';
 
-export default function Header() {
+export default function Header({ inViewElement }) {
     return (
         <header className="sticky top-0 z-50 flex items-center justify-center p-8 bg-background border-b">
             <NavigationMenu>
                 <NavigationMenuList className="flex gap-6 flex-wrap items-center justify-center">
                     <NavigationMenuItem>
-                        <NavigationMenuLink href="#" className="p-2 bg-secondary border-border border hover:text-accent">
+                        <NavigationMenuLink href="#" className={`p-2 hover:text-accent ${inViewElement === 'hero' ? 'bg-secondary border-border border' : ''}`}>
                             Home
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
-                        <NavigationMenuLink href="#about" className="p-2 hover:text-accent">
+                        {/* <NavigationMenuLink href="#about" className={`p-2 hover:text-accent ${aboutInView ? 'text-blue-500' : 'text-red-500'}`}> */}
+                        <NavigationMenuLink
+                            href="#about"
+                            className={`p-2 hover:text-accent ${inViewElement === 'about' ? 'bg-secondary border-border border' : ''}`}
+                        >
                             About
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
-                        <NavigationMenuLink href="#skills" className="p-2 hover:text-accent">
+                        <NavigationMenuLink
+                            href="#skills"
+                            className={`p-2 hover:text-accent ${inViewElement === 'skills' ? 'bg-secondary border-border border' : ''}`}
+                        >
                             Skills
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
-                        <NavigationMenuLink href="#projects" className="p-2 hover:text-accent">
+                        <NavigationMenuLink
+                            href="#projects"
+                            className={`p-2 hover:text-accent ${inViewElement === 'projects' ? 'bg-secondary border-border border' : ''}`}
+                        >
                             Projects
                         </NavigationMenuLink>
                     </NavigationMenuItem>
                     <NavigationMenuItem>
-                        <NavigationMenuLink href="#contact" className="p-2 hover:text-accent">
+                        <NavigationMenuLink
+                            href="#contact"
+                            className={`p-2 hover:text-accent ${inViewElement === 'contact' ? 'bg-secondary border-border border' : ''}`}
+                        >
                             Contact
                         </NavigationMenuLink>
                     </NavigationMenuItem>
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import LandingPage from './landingPage/landingPage';
 export default function Home() {
     return (
         <div>
-            <Header />
+            {/* <Header /> */}
             {/* <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start"></main> */}
             <LandingPage />
             <Footer />
