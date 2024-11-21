befca86ba4f06c3a4b730311efd346d7479a47e9
OneLessPuppy <21loganmeeks@gmail.com>
2024-11-21 15:48:33 -0800

    Navigation Drawer for mobile devices now closes after clicking any link.

--- src/app/header/header.js
+++ src/app/header/header.js
@@ -1,4 +1,7 @@
+'use client';
+
 import Image from 'next/image';
+import { useState } from 'react';
 import {
     NavigationMenu,
     NavigationMenuContent,
@@ -13,31 +16,51 @@ import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, Dr
 import { Menu, X } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
-function MyNavigationMenuList({ inViewElement, className = '' }) {
+function MyNavigationMenuList({ handleClick, inViewElement, className = '' }) {
     return (
         <NavigationMenuList className={cn('flex', className)}>
             <NavigationMenuItem>
-                <NavigationMenuLink href="#" className={`p-2  hover:text-accent rounded-md ${inViewElement === 'hero' ? 'bg-border' : ''}`}>
+                <NavigationMenuLink
+                    onClick={handleClick}
+                    href="#"
+                    className={`p-2  hover:text-accent rounded-md ${inViewElement === 'hero' ? 'bg-border' : ''}`}
+                >
                     Home
                 </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-                <NavigationMenuLink href="#about" className={`p-2  hover:text-accent rounded-md ${inViewElement === 'about' ? 'bg-border' : ''}`}>
+                <NavigationMenuLink
+                    onClick={handleClick}
+                    href="#about"
+                    className={`p-2  hover:text-accent rounded-md ${inViewElement === 'about' ? 'bg-border' : ''}`}
+                >
                     About
                 </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-                <NavigationMenuLink href="#skills" className={`p-2  hover:text-accent rounded-md ${inViewElement === 'skills' ? 'bg-border' : ''}`}>
+                <NavigationMenuLink
+                    onClick={handleClick}
+                    href="#skills"
+                    className={`p-2  hover:text-accent rounded-md ${inViewElement === 'skills' ? 'bg-border' : ''}`}
+                >
                     Skills
                 </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-                <NavigationMenuLink href="#projects" className={`p-2  hover:text-accent rounded-md ${inViewElement === 'projects' ? 'bg-border' : ''}`}>
+                <NavigationMenuLink
+                    onClick={handleClick}
+                    href="#projects"
+                    className={`p-2  hover:text-accent rounded-md ${inViewElement === 'projects' ? 'bg-border' : ''}`}
+                >
                     Projects
                 </NavigationMenuLink>
             </NavigationMenuItem>
             <NavigationMenuItem>
-                <NavigationMenuLink href="#contact" className={`p-2  hover:text-accent rounded-md ${inViewElement === 'contact' ? 'bg-border' : ''}`}>
+                <NavigationMenuLink
+                    onClick={handleClick}
+                    href="#contact"
+                    className={`p-2  hover:text-accent rounded-md ${inViewElement === 'contact' ? 'bg-border' : ''}`}
+                >
                     Contact
                 </NavigationMenuLink>
             </NavigationMenuItem>
@@ -46,13 +69,20 @@ function MyNavigationMenuList({ inViewElement, className = '' }) {
 }
 
 export default function Header({ inViewElement }) {
+    const [showDrawer, setShowDrawer] = useState(false);
+
+    const handleClick = () => {
+        console.log('clicked!');
+        setShowDrawer(false);
+    };
+
     return (
         <>
             <header className="sticky top-0 z-50 flex items-center justify-between sm:justify-center bg-background border-b px-6 py-4 sm:py-6">
                 <Image className="overflow-hidden z-0 w-12 sm:hidden" src="/Symbol-Light.svg" alt="Picture of Logan Meeks" width={48} height={48} />
                 <NavigationMenu>
                     <MyNavigationMenuList inViewElement={inViewElement} className="sm:gap-6 gap-0 flex-wrap items-center justify-center sm:flex hidden" />
-                    <Drawer direction="top">
+                    <Drawer open={showDrawer} onOpenChange={setShowDrawer} direction="top">
                         <DrawerTrigger className="sm:hidden">
                             <Menu size={36} />
                         </DrawerTrigger>
@@ -63,7 +93,7 @@ export default function Header({ inViewElement }) {
                                     <X className="absolute right-8 top-7" />
                                 </DrawerClose>
                             </DrawerTitle>
-                            <MyNavigationMenuList inViewElement={inViewElement} className="flex-col gap-6 py-8" />
+                            <MyNavigationMenuList handleClick={handleClick} inViewElement={inViewElement} className="flex-col gap-6 py-8" />
                         </DrawerContent>
                     </Drawer>
                 </NavigationMenu>
